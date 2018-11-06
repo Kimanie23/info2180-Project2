173a84d921cc64f0a24a7a78f0ce57455587ee5c
Kimanie23 <43970374+Kimanie23@users.noreply.github.com>
2018-11-06 18:21:18 -0500

    Update fifteen.js

--- fifteen.js
+++ fifteen.js
@@ -1,1 +1,8 @@
+const puzzleHover_Colour = '#009900';
+
+var puzzlearea = $("#puzzlearea");
+$("puzzlearea").children().css("background-color", "green");
+
+//<something goes here>.style.background = "yellow"
+
 
